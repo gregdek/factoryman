1421bb2834ef84092008b3db6830f1e574a37878
Greg DeKoenigsberg <gregdek@amazon.com>
2021-02-06 09:20:46 -0800

    Encode and decode as utf-8 strings

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 from flask import Flask
 from flask import render_template
 from flask import Response
@@ -13,6 +15,7 @@ pp = pprint.PrettyPrinter(indent=4)
 redis_host = "localhost"
 redis_port = 6379
 redis_password = ""
+r = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
 
 @app.route('/')
 def index():
@@ -21,17 +24,16 @@ def index():
 @app.route('/newgame')
 def newgame():
   gameid = str(uuid.uuid4())
-  r = redis.Redis()
   # New game. 
   # "set" starting values, and "sadd" values to Redis set for gameid.
   # TODO: put these into a newworker function.
-  r.set(gameid+":cash", 5000)
-  r.set(gameid+":wcount", 1)
+  r.set(gameid+":cash", "5000")
+  r.set(gameid+":wcount", "1")
   r.set(gameid+":active:0", "yes")
-  r.set(gameid+":x:0", 0)
-  r.set(gameid+":y:0", 0)
-  r.set(gameid+":dx:0", 0)
-  r.set(gameid+":dy:0", 0)
+  r.set(gameid+":x:0", "0")
+  r.set(gameid+":y:0", "0")
+  r.set(gameid+":dx:0", "0")
+  r.set(gameid+":dy:0", "0")
   r.sadd(gameid, gameid+":cash", gameid+":wcount", gameid+":active:0")
   r.sadd(gameid, gameid+":x:0", gameid+":y:0")
   r.sadd(gameid, gameid+":dx:0", gameid+":dy:0")
@@ -42,11 +44,13 @@ def newgame():
 
 @app.route('/state/<gameid>')
 def state(gameid):
-  r = redis.Redis()
   gamestatestr = ''
   gamestateset = r.smembers(gameid)
   for gamestateitem in gamestateset:
-    gamestatestr += "<p>" + str(gamestateitem) + str(r.get(gamestateitem)) + "</p>"
+    gamestatestr += "<p>"
+    gamestatestr += str(gamestateitem)
+    gamestatestr += str(r.get(gamestateitem))
+    gamestatestr += "</p>"
   #print(gamestatestr, file=sys.stderr)
   return gamestatestr, {'Content-Type': 'text/html'}
 
